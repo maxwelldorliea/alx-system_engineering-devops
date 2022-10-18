b6edc6349c7c948e5b4122a5ed58b8f666ccd901
Maxwell D. Dorliea <deboy170@gmail.com>
2022-10-18 23:59:50 +0000

    Added task 8 solution

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+$stdout.sync = true
+print ARGV[0].scan(/from:(\+?\d*)/).join
+print ","
+print ARGV[0].scan(/to:(\+?\d*)/).join
+print ","
+puts ARGV[0].scan(/flags:([:\-0-9]*)/).join
