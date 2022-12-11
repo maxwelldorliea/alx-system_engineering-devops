df571c3d039659b1f78ba730656bf0b9e2558c1e
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-11 21:31:52 +0000

    Added task 3 solution

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# make changes to our configuration file
+
+file_line {'password-less':
+path    => '/etc/ssh/ssh_config'
+line    => '    PasswordAuthentication no'
+replace => True
+}
+
+
+file_line {'private-key':
+path    => '/etc/ssh/ssh_config'
+line    => '    IdentityFile ~/.ssh/school'
+replace => True
+}
