82431f77c5d51cc5f275ee9d61fdf26bfca64165
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-14 01:12:25 +0000

    Added task 5 solution

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# perform a 301 redirect when querying /redirect_me.
+
+exec {'update host':
+command => '/usr/bin/apt update -y'
+}
+
+exec {'updgrade host':
+command => '/usr/bin/apt upgrade -y'
+}
+
+exec {'Install nginx':
+command => '/usr/bin/apt install nginx'
+}
+
+exec {'Change var www owner':
+command => '/usr/bin/chown -R $USER:$USER /var/www'
+}
+
+exec {'Make Homepage to display hello world':
+command => '/usr/bin/echo Hello World > /var/www/html/index.nginx-debian.html'
+}
+
+exec {'redirect permanently':
+command => '/usr/bin/sed -i "26i \\\tlocation /redirect_me {\n\t\t return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4"'
+}
