47501da227496c92d12cbedb2fc06fb64cefe8d2
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-14 02:52:00 +0000

    Fixed bug in task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -17,11 +17,11 @@ command => '/usr/bin/chown -R $USER:$USER /var/www'
 }
 
 exec {'Make Homepage to display hello world':
-command => '/usr/bin/echo Hello World > /var/www/html/index.nginx-debian.html'
+command => '/usr/bin/echo Hello World! > /var/www/html/index.nginx-debian.html'
 }
 
 exec {'redirect permanently':
-command => '/usr/bin/sed -i "26i\ \tlocation /redirect_me {\n\t\t return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}" /etc/nginx/sites-available/default'
+command => '/usr/bin/sed -i "26i\ \tlocation /redirect_me {\n\t\t return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;i\n\t}" /etc/nginx/sites-available/default'
 }
 
 exec {'restart nginx':
