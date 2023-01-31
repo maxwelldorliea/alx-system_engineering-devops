f38a032e069fca9d83b702d74b6cfae7e935130e
Maxwell D. Dorliea <deboy170@gmail.com>
2023-01-31 23:37:59 +0000

    Fixed bug in task 1 solution

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,13 +8,15 @@ def top_ten(subreddit):
     header = {
             'User-Agent': 'MyBot/1.0'
             }
+    param = {
+            'limit': 10
+            }
     url = 'https://reddit.com/r/{}/top.json'.format(subreddit)
-    response = requests.get(url, headers=header, allow_redirects=False)
+    response = requests.get(
+            url, params=param headers=header, allow_redirects=False)
     if response.status_code == 200:
         post_list = response.json()['data']['children']
-        for idx, post in enumerate(post_list):
+        for post in post_list:
             print(post['data']['title'])
-            if idx == 9:
-                break
     else:
         print('None')
